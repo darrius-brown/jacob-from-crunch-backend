e48d0febe80b4e06743cf48c9c2fd835be5cb977
darriub15 <darriusabrown@gmail.com>
2023-03-21 21:54:01 -0700

    Working on user auth

--- jacob/models.py
+++ jacob/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=100, default='name')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
