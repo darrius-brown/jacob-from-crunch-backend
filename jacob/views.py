e6744521e844d23d19fe3fae5f2f3d215d5fe37e
darriub15 <darriusabrown@gmail.com>
2023-07-12 00:52:45 -0700

    grab random workouts working

--- jacob/views.py
+++ jacob/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Exercise, Program
 from rest_framework import status
+from random import sample
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model, authenticate, logout, login
 from .serializers import UserSerializer, ExerciseSerializer, ProgramSerializer
@@ -30,6 +31,7 @@ class LoginView(TokenObtainPairView):
         refresh = RefreshToken.for_user(user)
 
         data = {
+           'username': user.username,
             'user_id': user.id,
             'email': user.email,
             'first_name': user.first_name,
@@ -49,18 +51,37 @@ class CreateExercise(generics.CreateAPIView):
    serializer_class = ExerciseSerializer
    permission_classes = [permissions.AllowAny]
 
-class CreateProgram(generics.CreateAPIView):
-   serializer_class = ProgramSerializer
-   permission_classes = [permissions.AllowAny]
+# class CreateProgram(generics.CreateAPIView):
+  #  serializer_class = ProgramSerializer
+  #  permission_classes = [permissions.AllowAny]
 
-   def post(self, request, user_id, format=None):
+  #  def post(self, request, user_id, format=None):
+  #       user_id = self.kwargs['user_id']
+  #       serializer = ProgramSerializer(data=request.data, context={'user_id': user_id})
+  #       if serializer.is_valid():
+  #           serializer.save()
+  #           return Response(serializer.data, status=status.HTTP_201_CREATED)
+  #       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CreateProgram(generics.CreateAPIView):
+    serializer_class = ProgramSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, user_id, format=None):
         user_id = self.kwargs['user_id']
+
+        # Get six random exercises
+        exercises = sample(list(Exercise.objects.filter(category='Lower')), 2)
+        exercise_ids = [exercise.id for exercise in exercises]
+
+        # Add exercise ids to request data
+        request.data['exercise'] = exercise_ids
+
         serializer = ProgramSerializer(data=request.data, context={'user_id': user_id})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class ExerciseListByCategory(generics.ListAPIView):
   serializer_class = ExerciseSerializer
   permission_classes = [permissions.AllowAny]
